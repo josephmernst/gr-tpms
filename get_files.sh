bb7179982be405c98cf1f62c9a0bd8b5158108b3
apluser <apluser@QKTcamp.jhuapl.edu>
2018-09-11 20:39:51 -0400

    added get_files

---
+++ get_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p working
+cd working
+wget https://code.vt.edu/cyber-teachers/tpms/uploads/9c5ceeba0f703be480fef2fb0529bb6f/data0.fc32
+wget https://code.vt.edu/cyber-teachers/tpms/uploads/54abfc73241f66bbd61751b7a8ff36c5/data1.fc32
+wget https://code.vt.edu/cyber-teachers/tpms/uploads/0f612cf3babb9867806aef3b493ded4c/data2.fc32
+wget https://code.vt.edu/cyber-teachers/tpms/uploads/9bc4d51829767fa28af381b9ce8e41ea/data3.fc32
+wget https://code.vt.edu/cyber-teachers/tpms/uploads/a543175ce6cd90c131f87ccd654eca19/data4.fc32
+wget https://code.vt.edu/cyber-teachers/tpms/uploads/f5d88244b1d356f337a0fd62c41da777/data5.fc32
+wget https://code.vt.edu/cyber-teachers/tpms/uploads/2cb7ae4a44063580b6db4bfbd3bccef0/data6.fc32
